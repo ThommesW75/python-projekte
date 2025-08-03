1a35359f3d97e714dfd87bc9dfb5e10271ec6188
Thomas Wulf <thommesw75@gmail.com>
2025-08-03 22:50:40 +0200

    My first Python Programm.

---
+++ hello_world.py
@@ -0,0 +1,11 @@
+"""
+My first public Python program.
+
+This program demonstrates the use of the print() function
+to display a message in the terminal. It also shows how to
+properly document a Python file.
+"""
+
+# This is a single-line comment.
+# The following line will print a greeting to the console.
+print("Hello, World!")
