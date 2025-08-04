a5dae16445084c315a8931f1e0108c236eb570cf
Thomas Wulf <thommesw75@gmail.com>
2025-08-04 19:38:01 +0200

    Variables

---
+++ variables.py
@@ -0,0 +1,23 @@
+"""
+This program demonstrates the use of variables and
+the basic data types: string, integer, and float.
+"""
+
+# --- Variable Assignments ---
+
+# A string variable to store the name of a server
+server_name = "web-server-01"
+
+# An integer variable to store the number of CPU cores
+cpu_cores = 4
+
+# A float variable to store the current server load
+current_load = 1.75
+
+# --- Using the variables ---
+
+# We can print the variables to see their values
+print("Server Information:")
+print(server_name)
+print(cpu_cores)
+print(current_load)
